85a4aac374e525258cbffb734a28be1b711f4709
pitydevil <45563787+pitydevil@users.noreply.github.com>
2019-12-29 22:00:31 +0700

    Add files via upload

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,16 +1,11 @@
 /*jshint esversion: 6 */
-
 window.onload = function() {
-  alert('new ');
-
     const useNodeJS = false;   // if you are not using a node server, set this value to false
     const defaultLiffId = "1653715977-2oYaypJa";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
 
-    // if node is used, fetch the environment variable and pass it to the LIFF method
-    // otherwise, pass defaultLiffId
     if (useNodeJS) {
         fetch('/send-id')
             .then(function(reqResponse) {
@@ -33,8 +28,6 @@ function initializeLiffOrDie(myLiffId) {
     if (!myLiffId) {
         alert('tidak pake id');
     } else {
-        alert('function ketrigger');
-        alert('masuk idnya');
           initializeLiff(myLiffId);
     }
 }
@@ -55,51 +48,28 @@ function initializeLiff(myLiffId) {
           alert('error di inisiasi LIFF');
         });
 }
-
 /**
  * Initialize the app by calling functions handling individual app components
  */
 function initializeApp() {
     // check if the user is logged in/out, and disable inappropriate button
     if (liff.isLoggedIn()) {
-      alert('sudah masuk');
-      keyboardObserver()
+      keyboardObserver();
     } else {
-        alert('error inisiasi APP');
+        alert('Login Line Terlebih Dahulu');
     }
 }
-
-// function ketrigger() {
-//   if (!liff.isInClient()) {
-//       sendAlertIfNotInClient();
-//       alert('tidak di client');
-//   } else {
-//     alert('dalam client');
-//     liff.sendMessages([{
-//         'type': 'text',
-//         'text': "You've successfully sent a message! Hooray!"
-//     }]).then(function() {
-//         window.alert('Message sent');
-//         alert('message berhasil');
-//     }).catch(function(error) {
-//         alert('message error');
-//         window.alert('Error sending message: ' + error);
-//     });
-//   }
-// }
 
 function keyboardObserver() {
   document.getElementById('lineButton').addEventListener('click', function() {
       if (!liff.isInClient()) {
           sendAlertIfNotInClient();
       } else {
-        alert('dalam client');
         liff.sendMessages([{
             'type': 'text',
             'text': "You've successfully sent a message! Hooray!"
         }]).then(function() {
             window.alert('Message sent');
-            alert('message berhasil');
         }).catch(function(error) {
             alert('message error');
             window.alert('Error sending message: ' + error);
