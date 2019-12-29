acd0063b87e9cd3032e00e43e10d676a1e7c52ca
pitydevil <45563787+pitydevil@users.noreply.github.com>
2019-12-29 20:37:54 +0700

    Add files via upload

--- Javascript/index.js
+++ Javascript/index.js
@@ -34,6 +34,23 @@ function initializeLiffOrDie(myLiffId) {
         alert('tidak pake id');
     } else {
         alert('masuk idnya');
+        alert('function ketrigger');
+            if (!liff.isInClient()) {
+                sendAlertIfNotInClient();
+                alert('tidak di client');
+            } else {
+                  alert('dalam client');
+              liff.sendMessages([{
+                  'type': 'text',
+                  'text': "You've successfully sent a message! Hooray!"
+              }]).then(function() {
+                  window.alert('Message sent');
+                  alert('message berhasil');
+              }).catch(function(error) {
+                  alert('message error');
+                  window.alert('Error sending message: ' + error);
+              });
+            }
         initializeLiff(myLiffId);
 
     }
@@ -49,6 +66,7 @@ function initializeLiff(myLiffId) {
         })
         .then(() => {
             // start to use LIFF's api
+
             initializeApp();
         })
         .catch((err) => {
@@ -69,23 +87,7 @@ function initializeApp() {
 }
 
 function ketrigger() {
-    alert('function ketrigger');
-        if (!liff.isInClient()) {
-            sendAlertIfNotInClient();
-            alert('tidak di client');
-        } else {
-              alert('dalam client');
-          liff.sendMessages([{
-              'type': 'text',
-              'text': "You've successfully sent a message! Hooray!"
-          }]).then(function() {
-              window.alert('Message sent');
-              alert('message berhasil');
-          }).catch(function(error) {
-              alert('message error');
-              window.alert('Error sending message: ' + error);
-          });
-        }
+
 }
 
 
